2b928305e35bc13c8fa5e63e24f6b350a0f70395
David-Sang96 <luainawl@gmail.com>
2024-06-16 17:27:27 +0800

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import "./index.css";
 import { persistor, store } from "./store/store.js";
-
-if (import.meta.env.VITE_MODE === "production") {
-  disableReactDevTools();
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
